42ba340a8ed9a90cce33cb2a935719ef6d5a76c8
vagrant <vagrant@homestead>
2018-04-15 04:57:39 +0000

    完成静态页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title','我的主页') -- laravel练习</title>
+</head>
+<body>
+   @yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title','主页')
+
+@section('content')
+    <h1>我的关于</h1>
+@stop
